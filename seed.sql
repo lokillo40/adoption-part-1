e07399238edc1e7dfc42875112d6e1213914d2cd
lokillo40 <123581754+lokillo40@users.noreply.github.com>
2023-08-02 13:08:31 -0400

    Add files via upload

---
+++ seed.sql
@@ -0,0 +1,25 @@
+
+DROP DATABASE IF EXISTS  adopt;
+
+CREATE DATABASE adopt;
+
+\c adopt
+
+CREATE TABLE pets
+(
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  species TEXT NOT NULL,
+  photo_url TEXT,
+  age INT,
+  notes TEXT,
+  available BOOLEAN NOT NULL DEFAULT TRUE
+);
+
+INSERT INTO pets
+  (name, species, photo_url, age, notes, available)
+VALUES
+  ('Woofly', 'dog', 'https://www.thesprucepets.com/thmb/hxWjs7evF2hP1Fb1c1HAvRi_Rw0=/2765x0/filters:no_upscale():strip_icc()/chinese-dog-breeds-4797219-hero-2a1e9c5ed2c54d00aef75b05c5db399c.jpg', 3, 'Incredibly adorable.', 't'),
+  ('Porchetta', 'porcupine', 'http://kids.sandiegozoo.org/sites/default/files/2017-12/porcupine-incisors.jpg', 4, 'Somewhat spiky!', 't'),
+  ('Snargle', 'cat', 'https://www.catster.com/wp-content/uploads/2017/08/A-fluffy-cat-looking-funny-surprised-or-concerned.jpg', null, null, 't'),
+  ('Dr. Claw', 'cat', null, null, null, 't');
